469b800576eda1837d4d392c69cf4e8eb95bbf02
Fred <fred.simard.code@gmail.com>
2020-04-05 16:44:28 -0400

    Adding animation

--- src/application/Controller.java
+++ src/application/Controller.java
@@ -7,13 +7,18 @@ import java.net.URL;
 import java.time.Duration;
 import java.util.ResourceBundle;
 
+import javax.sound.midi.Soundbank;
+
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXComboBox;
 import com.sun.prism.paint.Color;
 import com.sun.prism.paint.Paint;
 
 import javafx.animation.FadeTransition;
+import javafx.animation.PauseTransition;
+import javafx.animation.SequentialTransition;
 import javafx.animation.Timeline;
+import javafx.animation.TranslateTransition;
 import javafx.collections.ObservableList;
 import javafx.event.Event;
 import javafx.fxml.FXML;
@@ -54,6 +59,7 @@ public class Controller implements Initializable {
 	private JFXButton sortButton;
 	//Text menu
 	private Text text;
+	private SequentialTransition sq;
 	
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
@@ -69,7 +75,10 @@ public class Controller implements Initializable {
 		jfxComboBox.getItems().add(new Label("Quicksort"));
 		jfxComboBox.getItems().add(new Label("Heap sort"));
 		sortButton.setDisable(true);
-		
+		jfxComboBox.valueProperty().addListener(e -> {
+			if (sortButton.isDisable()) sortButton.setDisable(false);
+			translationOfSortButton();
+		});
 	}
 	//CSS doesnt work when hovering a text element
 	public void hoverInTextMenu(MouseEvent e) {
@@ -80,6 +89,45 @@ public class Controller implements Initializable {
 	public void hoverOutTextMenu(MouseEvent e) {
 		text = (Text) e.getSource();
 		text.setFill(javafx.scene.paint.Paint.valueOf("#ABB37B"));
+	}
+	public void translationOfSortButton() {
+		
+		TranslateTransition firstBounce = new TranslateTransition(javafx.util.Duration.seconds(0.2), sortButton);
+		firstBounce.setFromY(sortButton.getLayoutY() -19);
+		firstBounce.setToY(firstBounce.getFromY() -19);
+		firstBounce.setCycleCount(2);
+		firstBounce.setAutoReverse(true);
+		TranslateTransition secondTransition = new TranslateTransition(javafx.util.Duration.seconds(0.2), sortButton);
+		secondTransition.setFromY(sortButton.getLayoutY() -19);
+		secondTransition.setToY(secondTransition.getFromY() -10);
+		secondTransition.setCycleCount(2);
+		secondTransition.setAutoReverse(true);
+		TranslateTransition thirdTranslation = new TranslateTransition(javafx.util.Duration.seconds(0.2), sortButton);
+		thirdTranslation.setFromY(sortButton.getLayoutY() -19);
+		thirdTranslation.setToY(secondTransition.getFromY() -5);
+		thirdTranslation.setCycleCount(2);
+		thirdTranslation.setAutoReverse(true);
+		sq = new SequentialTransition();
+		sq.getChildren().addAll(firstBounce,secondTransition, thirdTranslation);
+		sq.setCycleCount(1);
+		sq.setOnFinished(e -> {
+			sq.setDelay(javafx.util.Duration.seconds(5));
+			sq.play();
+		});
+		sq.play();
+//		TranslateTransition transition = new TranslateTransition();
+//		
+//		transition.setDuration(javafx.util.Duration.seconds(0));
+//		transition.setNode(sortButton);
+//		transition.setFromY(sortButton.getLayoutY() -19);
+//		transition.setToY(transition.getFromY() -10);
+//		transition.setAutoReverse(true);
+//		transition.setCycleCount(2);
+//		transition.setOnFinished(e ->{
+//			transition.setDelay(javafx.util.Duration.seconds(1));
+//			transition.play();
+//		});
+//		transition.play();
 	}
 	public void fadeAnimation(MouseEvent e) {
 		FadeTransition ft = new FadeTransition(javafx.util.Duration.seconds(0.5), text);
