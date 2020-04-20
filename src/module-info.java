552f896705f15693b1af81555712a553313236ab
Fred <fred.simard.code@gmail.com>
2020-04-20 03:40:48 -0400

    Fix librairy

--- src/module-info.java
+++ src/module-info.java
@@ -1,6 +1,13 @@
 module JavaFXDesktopApplication {
-    requires javafx.fxml;
+
+    requires javafx.graphics;
     requires javafx.controls;
+    requires javafx.media;
+    requires javafx.base;
+    requires javafx.web;
+    requires javafx.swing;
+    requires javafx.fxml;
+
     requires com.jfoenix;
 
     opens application;
